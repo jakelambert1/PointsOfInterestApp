58ace9a94791384959aec47f171e72da28db5aab
0lambj41 <jakelambert1@hotmail.co.uk>
2017-04-24 15:31:22 +0100

    map

--- app/src/main/java/com/example/a0lambj41/pointsofinterestapp/MainActivity.java
+++ app/src/main/java/com/example/a0lambj41/pointsofinterestapp/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.a0lambj41.pointsofinterestapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
+public class MainActivity extends Activity
+{
+
+    MapView mv;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
+
+        // This line sets the user agent, a requirement to download OSM maps
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         setContentView(R.layout.activity_main);
+
+        mv = (MapView)findViewById(R.id.map1);
+
+        mv.setBuiltInZoomControls(true);
+        mv.getController().setZoom(14);
+        mv.getController().setCenter(new GeoPoint(50.9,-1.4));
     }
 }
