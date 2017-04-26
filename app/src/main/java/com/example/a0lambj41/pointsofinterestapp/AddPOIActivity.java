3a3d87672d73db320cd72b3d40db9442db19bc37
jakelambert1 <jakelambert1@hotmail.co.uk>
2017-04-26 15:24:52 +0100

    Add poi done

--- app/src/main/java/com/example/a0lambj41/pointsofinterestapp/AddPOIActivity.java
+++ app/src/main/java/com/example/a0lambj41/pointsofinterestapp/AddPOIActivity.java
@@ -5,13 +5,39 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
 
-public class AddPOIActivity extends Activity{
+public class AddPOIActivity extends Activity implements View.OnClickListener{
 
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mca);
+        setContentView(R.layout.add_poi);
+
+        Button marker = (Button)findViewById(R.id.addMarkerBtn);
+        marker.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+
+        EditText name = (EditText) findViewById(R.id.nameEditText);
+        EditText type = (EditText) findViewById(R.id.typeEditText);
+        EditText description = (EditText) findViewById(R.id.descriptionEditText);
+
+        String newname = name.getText().toString();
+        String newtype = name.getText().toString();
+        String newdescription = name.getText().toString();
+
+        bundle.putString("com.example.pointofinterestapp.name",newname);
+        bundle.putString("com.example.pointofinterestapp.type",newtype);
+        bundle.putString("com.example.pointofinterestapp.desc",newdescription);
+
+        intent.putExtras(bundle);
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
 
--- app/src/main/java/com/example/a0lambj41/pointsofinterestapp/MainActivity.java
+++ app/src/main/java/com/example/a0lambj41/pointsofinterestapp/MainActivity.java
@@ -7,16 +7,22 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity
 {
 
     MapView mv;
+    ItemizedIconOverlay<OverlayItem> items;
 
     /** Called when the activity is first created. */
     @Override
@@ -26,10 +32,14 @@ public class MainActivity extends Activity
         // This line sets the user agent, a requirement to download OSM maps
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         setContentView(R.layout.activity_main);
+
         mv = (MapView)findViewById(R.id.map1);
+
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(50.9,-1.4));
+
+        items = new ItemizedIconOverlay<OverlayItem>(this, new ArrayList<OverlayItem>(), null);
     }
 
     public boolean onCreateOptionsMenu(Menu menu)
@@ -44,8 +54,36 @@ public class MainActivity extends Activity
         if(item.getItemId() == R.id.addpoi)
         {
             // react to the menu item being selected...
+            // Launch second activity
+            Intent intent = new Intent(this,AddPOIActivity.class);
+            startActivityForResult(intent, 0);
             return true;
         }
         return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if(requestCode == 0) {
+            Bundle bundle = intent.getExtras();
+
+            String poiname = bundle.getString("com.example.pointofinterestapp.name");
+            String poitype = bundle.getString("com.example.pointofinterestapp.type");
+            String poidesc = bundle.getString("com.example.pointofinterestapp.desc");
+
+            double latitude = mv.getMapCenter().getLatitude();
+            double longitude = mv.getMapCenter().getLongitude();
+
+            OverlayItem addpoi = new OverlayItem(poiname, poitype + poidesc, new GeoPoint(latitude, longitude));
+
+            items.addItem(addpoi);
+            mv.getOverlays().add(items);
+            mv.refreshDrawableState();
+
+            Toast.makeText(MainActivity.this, "Marker Created!", Toast.LENGTH_SHORT).show();
+
+        }else if(requestCode == 1) {
+        }
     }
 }
